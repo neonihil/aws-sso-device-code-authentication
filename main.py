164053124bbabe0ff9fb39f9d90f3ce8e662762b
Daniel Kovacs <mondomhogynincsen@gmail.com>
2022-03-27 17:46:21 +1100

    bugfix: Only writing the sso token to the disk if it was freshly created

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ SSO_TOKEN_TIMEOUT=timedelta(hours=10)
 
 def main(args):
     aws_sso_token = None
+    aws_sso_token_reused = False
     if os.path.isfile(args.output_file):
         aws_sso_token_age = datetime.now() - datetime.fromtimestamp(os.path.getmtime(args.output_file))
         print(f"Found AWS SSO Token in file: {args.output_file}, and it is {aws_sso_token_age} old.")
         if aws_sso_token_age <= SSO_TOKEN_TIMEOUT:
             with open(args.output_file, 'r') as f:
                 aws_sso_token = f.read()
+                aws_sso_token_reused = True
         else:
             print(f"AWS Token file: {args.output_file} is older than {SSO_TOKEN_TIMEOUT}, requesting a new one...")
     if not aws_sso_token:
         aws_sso_token = retrieve_aws_sso_token(args)
 
-    if args.output_file:
+    if args.output_file and not aws_sso_token_reused:
         with open(args.output_file, 'w') as f:
             f.write(aws_sso_token)
             print(f"Wrote the AWS SSO token to {args.output_file}")
